ada278eeee785e777c376fdac05e0e288e5a1166
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-22 19:49:12 -0300

    Correção bug
    HTTP para HTTPS

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const apiKey = "b8aa1791";
 const imgDefault = "./default_image.png";
 
 async function buscaFilme(nomeBusca){
-  const resposta = await fetch(`http://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`); // http://www.omdbapi.com/?i=tt3896198&apikey=b8aa1791
+  const resposta = await fetch(`https://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`); // http://www.omdbapi.com/?i=tt3896198&apikey=b8aa1791
   return resposta.json();
 };
 
