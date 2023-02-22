b15668ccbf3b7c0e640ef4c33e6ada8616765b50
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-22 18:42:52 -0300

    Chaves de acesso HTML-DOM
    chaves criadas para acessar DOM

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const nomeBusca = document.querySelector(".input");
+const mensagemErro = document.querySelector("#mensagemErro");
+const botaoBuscar = document.querySelector("botão_buscar");
+const titulo = document.querySelector("#titulo");
+const ano = document.querySelector("#ano");
+const duracao = document.querySelector("#duracao");
+const genero = document.querySelector("#genero");
+const direto = document.querySelector("#direto");
+const atores = document.querySelector("#atores");
+const poster = document.querySelector("#poster");
+const sinopse = document.querySelector("#sinopse");
+
 const apiKey = "b8aa1791";
 const imgDefault = "./default_image.png";
 
